c177361b77eb89d181343f819f62c52688f8e912
cBinimelis <cristoferbinimelis@gmail.com>
2018-06-13 22:57:23 -0400

    Agregar archivos de proyecto.

---
+++ Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master Pages/MP_Guest.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <div class="uk-section uk-padding" style="background-color:#8BEAAF">
+        <h1 class="uk-light cLetter"><b>Bienvenido</b></h1>
+    </div>
+
+    <div class="uk-section seccion-diagonal" style="background-color: #ffffff">
+        <div class="fondo-diagonal" style="background-color: #8BEAAF;"></div>
+    </div>
+    <div class="uk-section uk-padding uk-text-center">
+            <h1 class="cLetter">Bienvenido</h1>
+
+        <div class="uk-container uk-align-center">
+            <a class="boton-kuki uk-button">Boton de Prueba 1</a>
+        </div>
+        <div class="uk-container uk-align-center" >
+            <a class="boton-ver uk-button">Boton de Prueba 2</a>
+        </div>
+    </div>
+</asp:Content>
+
---
+++ js/PersonalScript.js
@@ -0,0 +1,67 @@
+﻿function Alerta(tit, mens, stat) {
+    swal({
+        title: tit,
+        text: mens,
+        icon: stat,
+        dangerMode: true
+    })
+}
+
+
+$(window).on('load', function () {
+    $('body').toggleClass('loaded');
+    Contar();
+});
+
+function Contar() {
+    $('.count').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+                duration: 3000,
+                easing: 'swing',
+                step: function (now) {
+                    $(this).text(Math.ceil(now));
+                }
+            });
+    });
+}
+
+function Redirect(UrlPath) {
+    window.location.hef = UrlPath;
+}
+
+function Delete(ctl, event) {
+    var defaultAction = $(ctl).prop("href");
+    event.preventDefault();
+    swal({
+        title: "Para evitar errores...",
+        text: "¿Estás seguro de querer borrar esto?",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    }).then((willDelete) => {
+        if (willDelete) {
+            window.location.href = defaultAction;
+            return true;
+        } else {
+            swal({
+                title: "Estuvo bien preguntar",
+                text: "No se ha eliminado nada",
+                icon: "info"
+            });
+            return false;
+        }
+    });
+}
+
+function VPrevia(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+
+            document.getElementById("ContentPlaceHolder1_imagen_vprevia").setAttribute("src", e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
