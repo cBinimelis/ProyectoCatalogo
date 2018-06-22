f434b049392828f294085a656ea78548d94a2b5b
cBinimelis <cristoferbinimelis@gmail.com>
2018-06-22 12:17:39 -0400

    backend Login

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 public partial class Login : System.Web.UI.Page
 {
+    Conexion sql = new Conexion();
     protected void Page_Load(object sender, EventArgs e)
     {
 
@@ -16,7 +18,31 @@ public partial class Login : System.Web.UI.Page
     {
         try
         {
-            if()
+            if (txt_user.Text.Trim().Equals("") || txt_pass.Text.Trim().Equals(""))
+            {
+                Mensaje("¡Un momento!", "No puedes dejar elementos vacíos", "info");
+            }
+            else
+            {
+                String User = txt_user.Text;
+                String Pass = txt_pass.Text;
+                SqlDataReader Usuario = sql.consulta("SELECT * FROM Usuario WHERE Username = '" + User + "' OR Correo = '" + User + "'");
+                if (Usuario.Read())
+                {
+                    if (Usuario[2].ToString().Equals(Pass))
+                    {
+
+                    }
+                    else
+                    {
+                        Mensaje("¡Ups!", "La contraseña ingresada no es correcta", "warning");
+                    }
+                }
+                else
+                {
+                    Mensaje("Tenemos un problema", "Este usuario no está en el sistema", "warning");
+                }
+            }
         }
         catch
         {
---
+++ NavAdmin/Inicio.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master Pages/MP_Admin.master" AutoEventWireup="true" CodeFile="Inicio.aspx.cs" Inherits="NavAdmin_Inicio" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <h1>navegacion privada</h1>
+</asp:Content>
+
